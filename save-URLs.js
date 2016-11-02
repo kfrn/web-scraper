fe2a1c5dc6730ca19ce231371e144441abbdd324
kfrn <knfrances@gmail.com>
2016-11-02 17:27:46 +1300

    Complete saveURLs FN and call with test data

--- save-URLs.js
+++ save-URLs.js
@@ -2,6 +2,19 @@ var fs = require('fs')
 
 module.exports = saveURLs
 
-function saveURLs(parameter) {
-  
+function saveURLs(URLs) {
+  fs.writeFile('data.txt', URLs, function(err) {
+    if (err) throw error
+    console.log("Saved!");
+  })
 }
+
+
+// For Testing
+
+// var URLarray = [ 'index.html',
+//   'blog/c1-reflection-blog.html',
+//   'blog/c1-time-and-habits-blog.html',
+//   'blog/t2-html-css-dom-p1.html',
+//   'blog/t2-html-css-dom-p2.html' ]
+// saveURLs(URLarray)
