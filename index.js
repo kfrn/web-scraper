73eb02b9ffb732e4108170630778a8ceb7e82f5a
kfrn <knfrances@gmail.com>
2016-11-02 17:52:55 +1300

    Add specific parameters

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var getPage = require('./get-page')
 var extractURLs = require('./extract-URLs')
 var saveURLs = require('./save-URLs')
 
-getPage("http://www.imdb.com/search/title?count=50&countries=it&sound_mixes=silent&sort=release_date,asc")
+getPage("http://www.imdb.com/search/title?count=3000&countries=it&sound_mixes=silent&sort=release_date,asc")
   .then (function(result) {
     // console.log(result);
     var URLarray = extractURLs(result)
     console.log("Extracted URLs are:", extractURLs(result));
-    saveURLs(URLarray)
+    saveURLs(URLarray, 'URLs_01.txt')
   })
   .catch (function(error) {
     console.log(error);
