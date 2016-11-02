1cff4c925421e116adc0649719ec4efe9a169d8f
kfrn <knfrances@gmail.com>
2016-11-02 19:32:26 +1300

    rename saveURLs to saveData & correct references to it

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var getPage = require('./get-page')
 var extractURLs = require('./extract-URLs')
-var saveURLs = require('./save-URLs')
+var saveData = require('./save-data')
 
-getPage("http://www.imdb.com/search/title?count=3000&countries=it&sound_mixes=silent&sort=release_date,asc")
+getPage("http://www.imdb.com/search/title?count=200&countries=it&release_date=1923-11,&sound_mixes=silent&sort=release_date,asc&page=3&ref_=adv_nxt")
   .then (function(result) {
     // console.log(result);
     var URLarray = extractURLs(result)
     console.log("Extracted URLs are:", extractURLs(result));
-    saveURLs(URLarray, 'URLs_01.txt')
+    saveData(URLarray, 'URLs_08.txt')
   })
   .catch (function(error) {
     console.log(error);
---
+++ save-data.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+
+module.exports = saveData
+
+function saveData(data, filename) {
+  fs.writeFile(filename, data, function(err) {
+    if (err) throw error
+    console.log("Saved!");
+  })
+}
+
+
+// For Testing
+
+// var URLarray = [ 'index.html',
+//   'blog/c1-reflection-blog.html',
+//   'blog/c1-time-and-habits-blog.html',
+//   'blog/t2-html-css-dom-p1.html',
+//   'blog/t2-html-css-dom-p2.html' ]
+// saveURLs(URLarray)
