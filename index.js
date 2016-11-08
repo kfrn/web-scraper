be0136d0282eceaade8aeb30a1879e8664e30c0a
kfrn <knfrances@gmail.com>
2016-11-09 12:12:20 +1300

    Regroup FNs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var getPage = require('./get-page')
-var extractURLs = require('./extract-URLs')
-var saveData = require('./save-data')
+var getPage = require('./functions/get-page')
+var extractURLs = require('./functions/extract-URLs')
+var saveData = require('./functions/save-data')
 
-getPage("http://www.imdb.com/search/title?count=200&countries=it&release_date=1923-11,&sound_mixes=silent&sort=release_date,asc&page=3&ref_=adv_nxt")
+getPage("http://www.imdb.com/search/title?release_date=1895-01-01,1895-06-30&sort=year,asc&sound_mixes=silent")
   .then (function(result) {
     // console.log(result);
     var URLarray = extractURLs(result)
